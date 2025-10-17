dca626130bad966b813fb4dadf20565a5560643a
MarianaEstaTrabajando <l23121131@morelia.tecnm.mx>
2025-10-17 11:34:08 -0600

    Sudokus muy rústicos pero este código lo hice con mi imaginación

--- Cosa1/AlgoComoSudoku.py
+++ Cosa1/AlgoComoSudoku.py
@@ -1,29 +1,30 @@
-lista =[0]*72
-c=0; p=2; rep=0
-while (c<72):
-    if((c-4)%9==0):
+#Creo un algoritmo para generar un Sudoku 
+lista =[0]*72 #lista vacía 
+c=0; p=2; rep=0 #contador, número a poner, repeticiones de p
+while (c<72): #while para llenar la lista
+    if((c-4)%9==0): #posición del 5
         lista[c]=5
 
-    if(rep==2):
+    if(rep==2): #si ya se repitió p dos veces
         p+=2
         rep=0
-    if(c%9==0 ):
+    if(c%9==0 ): #si es inicio de fila
         lista[c]=p
         rep+=1
         num=int((15-p)/2)
-        if(num%2==0):
+        if(num%2==0): #si es par
             par=num
             imp=15-p-num
-        else:
+        else:      #si es impar
             imp=num
             par=15-p-num
-        if(imp==5):
+        if(imp==5): #si imp es 5
             imp+=2
             par-=2
-        if(par==p):
+        if(par==p): #si par es igual a p
             par+=4
             imp-=4
-        if((c/9)%2==0):
+        if((c/9)%2==0): #si es fila par
             lista[c+8]=15-p-5
             lista[c+1]=imp
             lista[c+2]=par
@@ -31,7 +32,7 @@ while (c<72):
             lista[c+3]=(15-5-lista[c+5])
             lista[c+6]=15-p-lista[c+3]
             lista[c+7]=15-lista[c+6]-lista[c+8]
-        else:
+        else:  #si es fila impar
             lista[c+8]=15-p-5
             lista[c+3]=imp
             lista[c+6]=par
@@ -42,7 +43,7 @@ while (c<72):
             
     c+=1
 h=0
-for i in lista:
+for i in lista: #imprimir sudoku
     if(h%3==0 and h%9!=0 and h!=0):
         print("\n")
     if(h%9==0 and h!=0):
