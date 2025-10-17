38c5dede5b7e77232b9c253b37ecd682326c82fc
MarianaEstaTrabajando <l23121131@morelia.tecnm.mx>
2025-10-17 12:27:34 -0600

    Seguimiento de un objeto de un determinado color

--- Cosa1/IntentoFeo3.py
+++ Cosa1/IntentoFeo3.py
@@ -1,27 +1,27 @@
 import cv2
 import numpy as np
-
+# Inicializar la captura de video desde la cámara web
 cap = cv2.VideoCapture(0)
-ls=set()
-cv2.waitKey(2000)
+ls=set() #Conjunto para almacenar las coordenadas de los píxeles detectados
+cv2.waitKey(2000) #Esperar 2 segundos para que la cámara se estabilice
 
 valor= True
-while cap.isOpened():
+while cap.isOpened(): #Mientras la cámara esté abierta
     ret, frame = cap.read()
     if not ret:
         break
     
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #Convertir la imagen a espacio de color HSV
     #. Definir el rango de color de la tela (verde, en este caso) en HSV
     lower_green = np.array([80, 80, 80])
     upper_green = np.array([140, 255, 255])
     
-    mask = cv2.inRange(hsv, lower_green, upper_green)
+    mask = cv2.inRange(hsv, lower_green, upper_green) #Crear una máscara para el color verde
     
-    res = cv2.bitwise_and(frame, frame, mask=mask)
+    res = cv2.bitwise_and(frame, frame, mask=mask) #Aplicar la máscara a la imagen original
     
-    alto=frame.shape[0]
-    ancho=frame.shape[1]
+    alto=frame.shape[0] #alto de la imagen
+    ancho=frame.shape[1] #ancho de la imagen
     for y in range(alto):     
         for x in range(ancho):
             if mask[y, x] != 0:
@@ -30,9 +30,9 @@ while cap.isOpened():
     
     #coords = np.column_stack(np.where(mask == 255))
     for i in ls: 
-        frame[i[0],i[1]]= [0,255,0]
+        frame[i[0],i[1]]= [0,255,0] #Pintar los pixeles detectados del color
     
-    cv2.imshow("Seguimiento", frame)
+    cv2.imshow("Seguimiento", frame) #Mostrar el video con el seguimiento
     cv2.imshow('mask', mask)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
